b4ac21fd558df432c4f01354765cc0de18cffb57
kihuni <stephenkihuni55@gmail.com>
2023-06-29 07:17:28 +0300

    solve some mistake

--- formatedPhoneNumber/formatedPhnNum.py
+++ formatedPhoneNumber/formatedPhnNum.py
@@ -2,7 +2,7 @@ def create_phone_number(n):
     if len(n) != 10:
         return "Invalid input: array must contain exactly 10 integers"
     
-    formatted_num = "(" + "".join(map(str, numbers[:3])) + ") " + "".join(map(str, numbers[3:6])) + "-" + "".join(map(str, numbers[6:]))
+    formatted_num = "(" + "".join(map(str, n[:3])) + ") " + "".join(map(str, n[3:6])) + "-" + "".join(map(str, n[6:]))
     return formatted_num
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
