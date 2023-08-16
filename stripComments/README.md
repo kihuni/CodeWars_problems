aa44edfdaf67cdd3024503310c70578c5a0f1de0
kihuni <stephenkihuni55@gmail.com>
2023-08-16 05:23:40 +0300

    added strip comments in a sring problem

---
+++ stripComments/README.md
@@ -0,0 +1,21 @@
+Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+The code would be called like so:
+
+result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+# result should == "apples, pears\ngrapes\nbananas"
+
+## Solution
+To achieve this solution, you can split the input string by lines, and for each line, check for the existence of any of the comment markers. Once you encounter a comment marker, you can strip the remainder of the line. [Here's how you can implement this:]()
---
+++ stripComments/stripComments.py
@@ -0,0 +1,29 @@
+def solution(string, markers):
+    # split the string into lines
+    lines = string.split('\n')
+    
+    # process each line
+    for line in range(len(lines)):
+        for marker in markers:
+            # if a marker us found, cut the string up to that marker
+            if marker in lines[line]:
+                lines[line] = lines[line].split(marker)[0]
+        # remove any trailling whitespace from the line
+        lines[line] = lines[line].rstrip()
+        
+    
+    # joine the processed lines back into a single string
+    return '\n'.join(lines)
+
+# Test the function
+result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+print(result)  # Expected output: "apples, pears\ngrapes\nbananas"
+
+
+
+
+
+
+
+            
+            
